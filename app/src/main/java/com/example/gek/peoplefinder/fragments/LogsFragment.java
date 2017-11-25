73e199234eb1ce55112b5636c312388f1a65c72a
Zhdanov Evgen <azakanych@healthprecision.com>
2017-11-25 21:26:07 +0200

    Added animation icon to FAB

--- app/src/main/java/com/example/gek/peoplefinder/fragments/LogsFragment.java
+++ app/src/main/java/com/example/gek/peoplefinder/fragments/LogsFragment.java
@@ -4,13 +4,16 @@ package com.example.gek.peoplefinder.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gek.peoplefinder.R;
+import com.example.gek.peoplefinder.helpers.Connection;
 import com.example.gek.peoplefinder.helpers.LogHelper;
 
 public class LogsFragment extends Fragment {
@@ -18,6 +21,7 @@ public class LogsFragment extends Fragment {
     private TextView tvLog;
     private FloatingActionButton fabRefresh;
     private LogHelper logHelper;
+    private AnimatedVectorDrawableCompat rotationAnim;
 
     public LogsFragment() {
         // Required empty public constructor
@@ -38,20 +42,33 @@ public class LogsFragment extends Fragment {
         tvLog = (TextView) rootView.findViewById(R.id.tvLog);
         tvLog.setText(logHelper.readLog());
 
+        rotationAnim = AnimatedVectorDrawableCompat.create(getActivity(), R.drawable.arrows_vector_animate);
+
         fabRefresh = (FloatingActionButton) rootView.findViewById(R.id.fabRefresh);
+        fabRefresh.setImageDrawable(rotationAnim);
         fabRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clickFab();
             }
         });
+
         return rootView;
     }
 
+    // TODO: 11/25/2017 This need move to AsyncTask
     private void clickFab(){
-        String log = logHelper.readLog();
-        if ((log != null) && (log.length() > 0)) {
-            tvLog.setText(log);
+        if (Connection.getInstance().isServiceRunning()){
+            fabRefresh.setClickable(false);
+            rotationAnim.start();
+            String log = logHelper.readLog();
+            if ((log != null) && (log.length() > 0)) {
+                tvLog.setText(log);
+            }
+            fabRefresh.setClickable(true);
+        } else {
+            Toast.makeText(getActivity(), "You must run service firstly", Toast.LENGTH_SHORT).show();
         }
+
     }
 }

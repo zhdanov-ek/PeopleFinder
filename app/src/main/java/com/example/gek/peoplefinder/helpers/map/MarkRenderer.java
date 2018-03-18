4e76dfff1f96e302a3c12f2d26d44452f4f3f641
Zhdanov Evgen <zhdanov.ek@gmail.com>
2018-03-18 23:44:23 +0200

    Fixed form and content of marks

--- app/src/main/java/com/example/gek/peoplefinder/helpers/map/MarkRenderer.java
+++ app/src/main/java/com/example/gek/peoplefinder/helpers/map/MarkRenderer.java
@@ -3,11 +3,11 @@ package com.example.gek.peoplefinder.helpers.map;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.gek.peoplefinder.R;
 import com.example.gek.peoplefinder.models.Mark;
@@ -29,10 +29,13 @@ import java.util.List;
 public class MarkRenderer extends DefaultClusterRenderer<Mark> {
     private IconGenerator mIconGenerator;
     private IconGenerator mClusterIconGenerator;
-    private final ImageView mImageView;
     private final ImageView mClusterImageView;
-    private final int mDimension;
+    private final int mHeight;
+    private final int mWidth;
     private Context mContext;
+
+    private final TextView mShortMarkNameTextView;
+    private final ImageView mIconMarkImageView;
 
     public static final String TAG = "RENDERER";
 
@@ -43,26 +46,28 @@ public class MarkRenderer extends DefaultClusterRenderer<Mark> {
         mIconGenerator = new IconGenerator(context);
         mClusterIconGenerator = new IconGenerator(context);
 
+        mHeight = (int) context.getResources().getDimension(R.dimen.custom_mark_height);
+        mWidth = (int) context.getResources().getDimension(R.dimen.custom_mark_width);
+
         View multiProfile = LayoutInflater.from(context).inflate(R.layout.multi_profile, null);
+        multiProfile.setLayoutParams(new ViewGroup.LayoutParams(mWidth, mHeight));
         mClusterIconGenerator.setContentView(multiProfile);
         mClusterImageView = (ImageView) multiProfile.findViewById(R.id.image);
 
-        mImageView = new ImageView(context);
-        mDimension = (int) context.getResources().getDimension(R.dimen.custom_profile_image);
-        mImageView.setLayoutParams(new ViewGroup.LayoutParams(mDimension, mDimension));
-        int padding = (int) context.getResources().getDimension(R.dimen.custom_profile_padding);
-        mImageView.setPadding(padding, padding, padding, padding);
-        mIconGenerator.setContentView(mImageView);
+        View markView = LayoutInflater.from(context).inflate(R.layout.one_profile, null);
+        mShortMarkNameTextView = (TextView) markView.findViewById(R.id.shortMarkNameTextView);
+        mIconMarkImageView = (ImageView) markView.findViewById(R.id.iconMarkImageView);
+        markView.setLayoutParams(new ViewGroup.LayoutParams(mWidth, mHeight));
 
-        Log.d(TAG, "MarkRenderer: mImageView " + mImageView.hashCode());
+        mIconGenerator.setContentView(markView);
     }
 
     @Override
     protected void onBeforeClusterItemRendered(Mark mark, MarkerOptions markerOptions) {
         // Draw a single person.
         // Set the info window to show their name.
-        mImageView.setImageResource(mark.isPerson() ? R.drawable.ic_man : R.drawable.ic_place);
-
+        mIconMarkImageView.setImageResource(mark.isPerson() ? R.drawable.ic_man : R.drawable.ic_place);
+        mShortMarkNameTextView.setText(mark.getName());
         Bitmap icon = mIconGenerator.makeIcon();
         markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon)).title(mark.getName());
     }
@@ -73,8 +78,8 @@ public class MarkRenderer extends DefaultClusterRenderer<Mark> {
         // Draw multiple people.
         // Note: this method runs on the UI thread. Don't spend too much time in here (like in this example).
         List<Drawable> profilePhotos = new ArrayList<Drawable>(Math.min(4, cluster.getSize()));
-        int width = mDimension;
-        int height = mDimension;
+        int width = mWidth;
+        int height = mHeight;
 
         for (Mark mark : cluster.getItems()) {
             // Draw 4 at most.
